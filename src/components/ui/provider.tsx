a8bb0a977258be12767de6073b39b53a60c3f41d
zakrzewskib <barzak200@gmail.com>
2026-02-06 16:33:40 +0100

    #6: Use customSystem theme

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,10 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { system } from '@chakra-ui/react/preset';
 import { ColorModeProvider, type ColorModeProviderProps } from './color-mode';
+import { customSystem } from './theme/customSystem';
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={system}>
+    <ChakraProvider value={customSystem}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
   );
--- src/components/ui/theme/customSystem.tsx
+++ src/components/ui/theme/customSystem.tsx
@@ -1,6 +1,6 @@
 import { createSystem, defaultConfig } from '@chakra-ui/react';
 
-export const system = createSystem(defaultConfig, {
+export const customSystem = createSystem(defaultConfig, {
   theme: {
     tokens: {
       fonts: {
