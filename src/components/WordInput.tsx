66e367a71d97440c6fa1379fcbc5f801c3089ac7
zakrzewskib <barzak200@gmail.com>
2026-02-20 12:01:00 +0100

    #8: Add checking if guess has correct length

--- src/components/WordInput.tsx
+++ src/components/WordInput.tsx
@@ -1,6 +1,7 @@
 import { Input } from '@chakra-ui/react';
 import { useState, type ChangeEvent, type SubmitEvent } from 'react';
 import { MOCK_CORRECT_WORD } from '../App';
+import { WORD_LENGTH } from '../constants';
 import type { IWord } from '../types';
 import { getGuessResult } from '../utils/gameLogic';
 
@@ -17,7 +18,14 @@ function WordInput(props: WordInputProps) {
 
   function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
-    // tbd: Add validation if word is correct
+
+    if (value.length !== WORD_LENGTH) {
+      // tbd: Add info about wrong format, for example some kind of error animation would be enough
+      return;
+    }
+
+    // tbd: Check if word exists in polish language
+
     const guess = value.toUpperCase();
     // tbd: Think if it's a backend side logic
     const result = getGuessResult(guess, MOCK_CORRECT_WORD);
