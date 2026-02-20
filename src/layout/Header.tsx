b816d1185af14291f672b0adc8abfa367b22e29a
zakrzewskib <barzak200@gmail.com>
2026-02-20 17:14:20 +0100

    #9: Add alt to header logo img

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
       justifyContent="space-between"
     >
       <HStack>
-        <Image src="/logo.webp" height="48px" />
+        <Image src="/logo.webp" height="48px" alt="Logo gry walka na słowa" />
         <Text as="h1" textStyle={{ base: 'md', sm: 'xl' }} fontWeight="bold">
           Walka na słowa
         </Text>
