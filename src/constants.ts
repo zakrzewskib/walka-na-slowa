155e8048ba87e1fc047cbc9b2848872afb44ba40
“zakrzewskib” <barzak200@gmail.com>
2026-08-28 09:46:54 +0200

    #10: Add polish letter and letters used types

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,22 @@
+import type { LettersUsed, PolishLetter } from './types';
+
 export const WORD_LENGTH = 5;
 export const WORDS_LENGTH = 6;
 export const HIDDEN_LETTER = '-';
 export const MOCK_CORRECT_WORD = 'LALKA';
+
+// prettier-ignore
+export const POLISH_ALPHABET: PolishLetter[] = [
+  'q','w','e','r','t','y','u','i','o','p',
+  'a','s','d','f','g','h','j','k','l',
+  'z','x','c','v','b','n','m',
+  'ą','ć','ę','ł','ń','ó','ś','ź','ż',
+]
+
+export const STARTING_LETTERS_USED: LettersUsed = POLISH_ALPHABET.reduce(
+  (acc, key) => {
+    acc[key] = { status: 'unused' };
+    return acc;
+  },
+  {} as LettersUsed,
+);
--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,16 @@ export interface IWord {
   id: string;
   letters: ILetter[];
 }
+
+// prettier-ignore
+export type PolishLetter =
+  | 'q' | 'w' | 'e' | 'r' | 't' | 'y' | 'u' | 'i' | 'o' | 'p'
+  | 'a' | 's' | 'd' | 'f' | 'g' | 'h' | 'j' | 'k' | 'l'
+  | 'z' | 'x' | 'c' | 'v' | 'b' | 'n' | 'm'
+  | 'ą' | 'ć' | 'ę' | 'ł' | 'ń' | 'ó' | 'ś' | 'ź' | 'ż';
+
+export type LetterStatus = 'unused' | 'correct' | 'present' | 'absent';
+
+export type LettersUsed = {
+  [K in PolishLetter]: { status: LetterStatus };
+};
