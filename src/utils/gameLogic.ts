3b2d395cc4baf8ed3db3d8353b113a780494ffb1
“zakrzewskib” <barzak200@gmail.com>
2026-09-11 12:25:28 +0200

    #21: Fix typo

--- src/utils/gameLogic.ts
+++ src/utils/gameLogic.ts
@@ -1,8 +1,8 @@
 import { v4 as uuidv4 } from 'uuid';
 import type { Letter, Word } from '../types';
 
-export function getWordResult(rawGuess: string, rawCorrectWorld: string): Word {
-  const correctWord = rawCorrectWorld.toUpperCase();
+export function getWordResult(rawGuess: string, rawCorrectWord: string): Word {
+  const correctWord = rawCorrectWord.toUpperCase();
   const guess = rawGuess.toUpperCase();
 
   if (!guess || guess.length === 0) {
